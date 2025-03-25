584c1fc7852a9d66a46a367fa7ea814588e29171
jjaeni <jjaeeun3203@naver.com>
2025-03-26 02:58:10 +0900

    Delete sql files

--- 프로그래머스/1/131112. 강원도에 위치한 생산공장 목록 출력하기/강원도에 위치한 생산공장 목록 출력하기.sql
+++
@@ -1,5 +0,0 @@
--- 코드를 입력하세요
-SELECT factory_id, factory_name, address
-from food_factory
-where instr(tlno, 033) > 0
-order by factory_id asc
--- 프로그래머스/1/293258. 잔챙이 잡은 수 구하기/잔챙이 잡은 수 구하기.sql
+++
@@ -1,4 +0,0 @@
--- 코드를 작성해주세요
-select count(*) as fish_count
-from fish_info
-where length is null
