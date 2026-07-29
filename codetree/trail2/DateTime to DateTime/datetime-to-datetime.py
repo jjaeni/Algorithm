0742664fc22d31e07be1c271e26c3e6da11075d1
Jaeeun Kang <101173154+jjaeni@users.noreply.github.com>
2026-07-30 01:18:21 +0900

    [Passed] DateTime to DateTime (47ms, 16MB)

--- codetree/trail2/DateTime to DateTime/datetime-to-datetime.py
+++ codetree/trail2/DateTime to DateTime/datetime-to-datetime.py
@@ -3,7 +3,7 @@ A, B, C = map(int, input().split())
 cond = (11*24*60) + (11*60) + 11
 cal = (A*24*60) + (B*60) + C
 
-if A<11 or (A==11 and B<11) or (A==11 and B==11 and C<11):
+if (cal-cond)<0:
     print(-1)
 else:
     print(cal-cond)
