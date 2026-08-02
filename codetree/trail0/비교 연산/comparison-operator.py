696a3e6b9b5d40985400028759aef6494df5f0ea
Jaeeun Kang <101173154+jjaeni@users.noreply.github.com>
2026-08-02 21:23:15 +0900

    [Passed] 비교 연산 (48ms, 16MB)

--- codetree/trail0/비교 연산/comparison-operator.py
+++ codetree/trail0/비교 연산/comparison-operator.py
@@ -1,24 +1,3 @@
 A, B = map(int, input().split())
 
-if A>=B:
-    print(1)
-    if A>B:
-        print(1)
-    else:
-        print(0)
-else:
-    print(0, 0, sep='\n')
-
-if A<=B:
-    print(1)
-    if A<B:
-        print(1)
-    else:
-        print(0)
-else:
-    print(0, 0, sep='\n')
-
-if A==B:
-    print(1, 0, sep='\n')
-else:
-    print(0, 1, sep='\n')
+print(int(A>=B), int(A>B), int(A<=B), int(A<B), int(A==B), int(A!=B), sep='\n')
