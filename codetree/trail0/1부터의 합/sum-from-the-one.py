eec2ec269e62b8af6e5eb43d0444747485bcf156
Jaeeun Kang <101173154+jjaeni@users.noreply.github.com>
2026-08-07 23:44:28 +0900

    [Passed] 1부터의 합 (45ms, 16MB)

--- codetree/trail0/1부터의 합/sum-from-the-one.py
+++ codetree/trail0/1부터의 합/sum-from-the-one.py
@@ -1,6 +1,8 @@
 N = int(input())
+num = 0
 
-for x in range(2, 102):
-    if sum(list(range(1, x))) >= N:
-        print(list(range(1, x))[-1])
+for x in range(1, 101):
+    num += x
+    if num >= N:
+        print(x)
         break
